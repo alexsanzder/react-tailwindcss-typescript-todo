63d8b974b514df6fb58b883089404fd858e0d90e
Alejandro Sánchez <alex.sanzb@gmail.com>
2021-07-26 23:25:08 +0200

    Adde urql proivder

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createClient, Provider } from "urql";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = createClient({
+    url: "https://graphqlzero.almansi.me/api",
+});
+
+ReactDOM.render(
+    <Provider value={client}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
